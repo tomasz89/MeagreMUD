9bbacdb0abfa2ed864a8345abb4690be7f7cdbe6
Tomasz Grzegurzko <tomasz89@gmail.com>
2018-07-21 15:14:40 +0930

    Basic project

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include <QApplication>
-#include "mainwindow.h"
-
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-
-    MeagreMud::MainWindow meagreMud;
-    meagreMud.show();
-
-    return app.exec();
+    return 0;
 }
